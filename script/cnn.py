36598ed31a8293a9b71baeabaa8be797f0288c22
rcrupi <crupi.riccardo@spes.uniud.it>
2023-02-07 00:10:50 +0100

    Add CNN model for redshift regression starting from GRB images.

---
+++ script/cnn.py
@@ -0,0 +1,178 @@
+# Standard package
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.image
+import matplotlib.cm
+# from PIL import Image
+import pandas as pd
+import pickle
+from tqdm import tqdm
+# local import
+from config.config import PATH_GRB_PKL, PATH_GRB_IMG
+from utils.read_data import build_dataset_gbm_z
+# nn
+import tensorflow as tf
+
+np.random.seed(42)
+
+df_tot_z = build_dataset_gbm_z()
+
+LEN_LC=512
+n_load=None
+bln_save_img_scale=True
+bln_augment=False
+scaler='Standard'
+cmap='Greys_r'
+min_amp=5.0
+norm=False
+
+lst_tte_pkl_aug = []
+np.random.seed(10)
+counter_set = 0
+
+counter_constant = 0
+# List of all GRB background subtracted and saved as a 2D array
+list_tte_pkl = [i for i in os.listdir(PATH_GRB_PKL) if 'pickle' in i if i[2:11] in df_tot_z['name'].str.slice(3).to_list()]
+n_ev = len(list_tte_pkl)
+list_tte_pkl_train = np.random.choice(list_tte_pkl, int(n_ev*0.8), replace=False)
+list_tte_pkl_residual = [i for i in list_tte_pkl if i not in list_tte_pkl_train]
+n_ev_res = len(list_tte_pkl_residual)
+list_tte_pkl_val = np.random.choice(list_tte_pkl_residual, int(n_ev_res*0.5), replace=False)
+list_tte_pkl_test = [i for i in list_tte_pkl_residual if i not in list_tte_pkl_val]
+
+
+def load_set(list_tte_pkl):
+    lst_tte_pkl_aug = []
+    ds_train = []
+    for i in tqdm(list_tte_pkl):
+        with open(PATH_GRB_PKL + i, 'rb') as f:
+            event_tmp = pickle.load(f)
+            if abs(event_tmp.max() - event_tmp.min()) < 10 ** -6:
+                print("event constant.")
+                continue
+            ds_train.append(event_tmp)
+            lst_tte_pkl_aug.append(' '.join(i.split('_')) + " " + "no shift")
+            if bln_augment:
+                event_tmp_r_shift = np.roll(event_tmp, np.random.randint(0, int(LEN_LC / 4), 1)[0], axis=0)
+                ds_train.append(event_tmp_r_shift)
+                lst_tte_pkl_aug.append(' '.join(i.split('_')) + " " + "right shift")
+                event_tmp_l_shift = np.roll(event_tmp, -np.random.randint(0, int(LEN_LC / 4), 1)[0], axis=0)
+                ds_train.append(event_tmp_l_shift)
+                lst_tte_pkl_aug.append(' '.join(i.split('_')) + " " + "left shift")
+    ds_train = np.array(ds_train)
+    return ds_train, lst_tte_pkl_aug
+
+ds_train, lst_train = load_set(list_tte_pkl_train)
+ds_test, lst_test = load_set(list_tte_pkl_test)
+ds_val, lst_val = load_set(list_tte_pkl_val)
+y_train = np.array([df_tot_z.loc[df_tot_z['name'].str.slice(3) == i[2:11], 'z'].values[0] for i in lst_train])
+y_test = np.array([df_tot_z.loc[df_tot_z['name'].str.slice(3) == i[2:11], 'z'].values[0] for i in lst_test])
+y_val = np.array([df_tot_z.loc[df_tot_z['name'].str.slice(3) == i[2:11], 'z'].values[0] for i in lst_val])
+
+col_list = ['t90', 't90_error', 't50', 't50_error',
+ 'flux_64', 'flux_64_error', 'fluence', 'fluence_error',
+ 'pflx_band_phtfluxb', 'flnc_band_ampl', 'pflx_band_epeak', 'flnc_band_phtfluxb',
+ 'pflx_band_ergflux', 'flnc_band_phtflux', 'pflx_band_beta', 'flnc_band_ergflux',
+ 'pflx_band_phtflux', 'flnc_band_beta', 'flnc_band_epeak', 'pflx_band_ampl',
+ 'pflx_band_alpha', 'flnc_band_alpha']
+
+df_tot_z.loc[:, col_list] = df_tot_z.loc[:, col_list].fillna(-1)
+add_train = np.array([df_tot_z.loc[df_tot_z['name'].str.slice(3) == i[2:11], col_list].values[0] for i in lst_train])
+add_test = np.array([df_tot_z.loc[df_tot_z['name'].str.slice(3) == i[2:11], col_list].values[0] for i in lst_test])
+add_val = np.array([df_tot_z.loc[df_tot_z['name'].str.slice(3) == i[2:11], col_list].values[0] for i in lst_val])
+
+# mapper from 2d array to rgba image
+sm = matplotlib.cm.ScalarMappable(cmap)
+
+# Scale dataset
+from sklearn.preprocessing import StandardScaler
+
+ds_train_scale = ds_train.copy()
+for i in tqdm(range(0, ds_train.shape[0])):
+    # Standard scaler
+    scaler = StandardScaler()
+    ds_train_scale[i, :, :] = scaler.fit_transform(ds_train[i, :, :])
+    ds_train_max = ds_train_scale[i, :, :].max()
+    ds_train_min = ds_train_scale[i, :, :].min()
+    if abs(ds_train_max - ds_train_min) > 10 ** -6:
+        ds_train_scale[i, :, :] = (ds_train_scale[i, :, :] - ds_train_min) / \
+                                  (max(min_amp, ds_train_max) - ds_train_min)
+
+ds_test_scale = ds_test.copy()
+for i in tqdm(range(0, ds_test.shape[0])):
+    # Standard scaler
+    ds_test_scale[i, :, :] = scaler.transform(ds_test[i, :, :])
+    ds_test_max = ds_train_scale[i, :, :].max()
+    ds_test_min = ds_train_scale[i, :, :].min()
+    if abs(ds_test_max - ds_test_min) > 10 ** -6:
+        ds_test_scale[i, :, :] = (ds_test_scale[i, :, :] - ds_test_min) / \
+                                  (max(min_amp, ds_test_max) - ds_test_min)
+
+ds_val_scale = ds_val.copy()
+for i in tqdm(range(0, ds_val.shape[0])):
+    # Standard scaler
+    ds_val_scale[i, :, :] = scaler.transform(ds_val[i, :, :])
+    ds_val_max = ds_val_scale[i, :, :].max()
+    ds_val_min = ds_val_scale[i, :, :].min()
+    if abs(ds_val_max - ds_val_min) > 10 ** -6:
+        ds_val_scale[i, :, :] = (ds_val_scale[i, :, :] - ds_val_min) / \
+                                  (max(min_amp, ds_val_max) - ds_val_min)
+
+# # # CNN model
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+input_2 = tf.keras.layers.Input(shape=(22))
+input_1 = tf.keras.layers.Input(shape=(512, 128, 1))
+model = layers.Conv2D(64, (3, 3), activation='relu', input_shape=(512, 128, 1))(input_1)
+mdoel = layers.MaxPooling2D((2, 2))(model)
+model = layers.Dropout(0.01)(model)
+model = layers.Conv2D(32, (3, 3), activation='relu')(model)
+model = layers.MaxPooling2D((2, 2))(model)
+model = layers.Dropout(0.01)(model)
+model = layers.Conv2D(32, (3, 3), activation='relu')(model)
+model = layers.MaxPooling2D((2, 2))(model)
+model = layers.Dropout(0.01)(model)
+model = layers.Flatten()(model)
+model = layers.Dense(64, activation='relu')(model)
+model = layers.Dropout(0.01)(model)
+concatenated = tf.keras.layers.concatenate([model, input_2])
+model_final = tf.keras.layers.Dense(32, activation='softmax')(concatenated)
+model_final = layers.Dropout(0.01)(model_final)
+out = tf.keras.layers.Dense(1, activation='relu', name='output_layer')(model_final)
+
+model = tf.keras.Model([input_1, input_2], out)
+
+model.summary()
+
+opt = tf.keras.optimizers.experimental.Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07,
+                                             weight_decay=None, clipnorm=None, clipvalue=None, global_clipnorm=None,
+                                             use_ema=False, ema_momentum=0.99, ema_overwrite_frequency=None,
+                                             jit_compile=True, name='Nadam')
+
+model.compile(optimizer=opt,
+              loss=tf.keras.losses.MSE,
+              metrics=['mse'])
+
+history = model.fit([ds_train_scale, add_train], y_train, epochs=8,
+                    validation_data=([ds_val_scale, add_val], y_val), batch_size=16)
+
+plt.plot(history.history['mse'], label='mse')
+plt.plot(history.history['val_mse'], label='val_mse')
+plt.xlabel('Epoch')
+plt.ylabel('mse')
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate([ds_test_scale, add_test], y_test, verbose=2)
+print(test_acc)
+
+fig = plt.figure(figsize=(10, 10))
+plt.scatter(y_train, model.predict(ds_train_scale), label='train')
+plt.scatter(y_test, model.predict(ds_test_scale), label='test')
+plt.plot([0,8],[0,8])
+plt.xlabel("true")
+plt.ylabel("predicted")
+plt.legend()
+plt.show()
+
--- script/redshift_regression.py
+++ script/redshift_regression.py
@@ -1,78 +1,10 @@
 # from gbm.finder import BurstCatalog
 import pandas as pd
 import numpy as np
-from config.config import GBM_BURST_DB, GRB_REDSHIFT
+from utils.read_data import build_dataset_gbm_z
 
-# Load Burst GRB catalog of Fermi GBM
-df_grb = pd.read_csv(GBM_BURST_DB)
-print(df_grb.shape)
-print(df_grb.head())
-
-# Load data redshift GRB https://www.mpe.mpg.de/~jcg/grbgen.html
-df_red = pd.read_csv(GRB_REDSHIFT, sep=';', encoding='latin')
-# correct position for a bugged row
-df_red.loc[df_red['GRBa'] == '190810AS', 'GRB X-ray position'] = "12h48m12sÿ+28ø 30'"
-df_red['GRBa'] = df_red['GRBa'].apply(lambda x: x.replace('S', ''))
-# 0 other S inside the name
-if df_red.loc[df_red['GRBa'].str.contains('S'), 'GRBa'].str.slice(0, -1).str.contains('S').sum() > 0:
-    print("warning: 'S' in name not at last.")
-
-
-def redshift_clean(x):
-    try:
-        res = None
-        if x == ' ' or x == '':
-            res = None
-        elif not pd.isna(x):
-            if 'ph' in x:
-                x = x.replace('ph', '')
-            if '?' in x:
-                x = x.replace('?', '')
-            if '<' in x:
-                x = x.replace('<', '')
-            if '-' in x:
-                x = x.split('-')
-            if not isinstance(x, list):
-                res = float(str(x).strip())
-            else:
-                res = (float(str(x[0]).strip()) + float(str(x[1]).strip()))/2
-    except Exception as e:
-        print(e)
-        print('Failed to convert Redshift: ', x, "<---- what is this???")
-        res = np.nan
-    return res
-
-
-# Clean the redshift values
-df_red['z'] = df_red['zc'].apply(lambda x: redshift_clean(x))
-df_red['GRB'] = df_red['GRBa']
-# Get localization
-df_red['ra'] = df_red['GRB X-ray position'].str.slice(0, 2).astype(int)/24*360
-df_red['dec'] = df_red['GRB X-ray position'].apply(lambda x: x.split('ÿ')[1][0:3]).astype(int, errors='ignore')
-# Selection only GRB name, redshift and localization
-df_red = df_red[['GRB', 'z', 'ra', 'dec']]
-# Define the "short" GRB name without the last letter or the last three number
-df_grb['name_short'] = df_grb['name'].apply(lambda x: x[3:9])
-df_red['name_short'] = df_red['GRB'].apply(lambda x: x[:-1] if x[-1].isalpha() else x)
-# Merge the dataframes
-df_tot_raw = df_grb.merge(df_red, on=['name_short'])
-df_tot_raw_z = df_tot_raw[df_tot_raw['z'].notna()].copy()
-# Define the difference in degree in localization
-df_tot_raw_z.loc[:, 'diff_degree'] = abs(df_tot_raw_z['ra_x'] - df_tot_raw_z['ra_y']).values +\
-                                     abs(df_tot_raw_z['dec_x'] - df_tot_raw_z['dec_y']).values
-# Take the minimum degree difference
-df_grb_diff_min = df_tot_raw_z.groupby('GRB', sort=True)['diff_degree'].min().reset_index()
-# Choose the GRB with the miniumum degree difference
-df_tot_z = df_grb_diff_min.merge(df_tot_raw_z, on=['GRB', 'diff_degree'], how='left')
-# Selection only a maximum difference of 50°
-df_tot_z = df_tot_z[df_tot_z['diff_degree'] < 50]
-df_tot_z = df_tot_z.loc[df_tot_z.groupby('name').diff_degree.idxmin()].reset_index(drop=True)
-# Duplicated events
-print(df_tot_z.loc[df_tot_z.name.duplicated(keep=False),
-                   ['name', 'trigger_time', 'diff_degree', 'ra_x', 'dec_x', 'ra_y', 'dec_y', 'z', 'GRB']])
-print(df_tot_z.loc[df_tot_z.GRB.duplicated(keep=False),
-                   ['name', 'trigger_time', 'diff_degree', 'ra_x', 'dec_x', 'ra_y', 'dec_y', 'z', 'GRB']])
-print(df_tot_z.shape)
+# Build the merged dataset
+df_tot_z = build_dataset_gbm_z()
 
 # # # Regression model
 from sklearn.impute import KNNImputer
